68cfc7738308100ad72ebac9b92ee80f4cef106d
hato-taka <84255881+hato-taka@users.noreply.github.com>
2023-11-06 20:20:47 +0900

    [add] addToCartBtnの実装

--- src/app/components/item.jsx
+++ src/app/components/item.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import { ShopContext } from "@/context/shop-context";
+import React, { useContext } from "react";
 
-function Item({ id, title, image, price, description, category }) {
+function Item(props) {
+  const { id, title, image, price, description, category } = props;
+  const { addToCart, cartItems } = useContext(ShopContext);
+  const cartItemCount = cartItems[id];
+
   return (
     <div key={id} className="product">
       <div className="content">
@@ -12,7 +17,9 @@ function Item({ id, title, image, price, description, category }) {
         <p className="description">{description}</p>
         <p className="category">{category}</p>
       </div>
-      <button className="addToCartBtn">ADD</button>
+      <button className="addToCartBtn" onClick={() => addToCart(id)}>
+        カートに追加する {cartItemCount > 0 && <span>({cartItemCount}個)</span>}
+      </button>
     </div>
   );
 }
